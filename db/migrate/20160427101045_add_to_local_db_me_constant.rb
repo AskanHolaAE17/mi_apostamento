f9ec9f34a3e095e6a5deeb67fc76b1bbf135f829
VitalinaLiane <vitalina.liane@gmail.com>
2016-04-30 21:51:43 +0300

    SandboxPay; RightMailing

--- db/migrate/20160427101045_add_to_local_db_me_constant.rb
+++
@@ -1,6 +0,0 @@
-class AddToLocalDbMeConstant < ActiveRecord::Migration
-
-  MeConstant.create title: 'site_title', content: 'site_title'
-  MeConstant.create title: 'site_title', content: 'site_title'
-  
-end
