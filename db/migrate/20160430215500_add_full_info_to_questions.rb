9b15275a5116726bfb6c765c45d3a15683c5e6b0
VitalinaLiane <vitalina.liane@gmail.com>
2016-04-30 21:54:18 +0300

    AddFullInfoToQuestions

---
+++ db/migrate/20160430215500_add_full_info_to_questions.rb
@@ -0,0 +1,14 @@
+class AddFullInfoToQuestions < ActiveRecord::Migration
+
+  Question.all.each do |f|
+    f.update_attribute :test_id, '1'
+  end
+  
+  @count = 1
+  Question.all.each do |f|
+    f.update_attribute :number_of_question, @count
+    @count = @count + 1
+  end
+
+end
+
