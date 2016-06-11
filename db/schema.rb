dd00ae4cc2bc99a3f42b739dfd88ce88cca58595
VitalinaLiane <vitalina.liane@gmail.com>
2016-06-11 13:21:20 +0300

    Add all Test2Level Info to Questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611005600) do
+ActiveRecord::Schema.define(version: 20160611130900) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
