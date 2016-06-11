f119916ce499cc7abd2c57935882bacc50991bd5
VitalinaLiane <vitalina.liane@gmail.com>
2016-06-11 16:05:59 +0300

    ---All Q [Test2 ReEnter Works]

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -70,7 +70,7 @@ class TestsController < ApplicationController
 #_______________________________________________________________________________            
     
           
-    questions = Test.find_by(number_of_test: test_number).questions.limit(3)      
+    questions = Test.find_by(number_of_test: test_number).questions
     #.limit(2)      
     
     
