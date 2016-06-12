ca1ec37e25e70b52ce0db704422c304544cf5a1e
VitalinaLiane <vitalina.liane@gmail.com>
2016-06-12 03:49:45 +0300

    Without BinaryField in DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612023800) do
+ActiveRecord::Schema.define(version: 20160612034700) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 20160612023800) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "image"
-    t.binary   "image_data"
     t.string   "level"
     t.string   "level_test_info"
   end
