2d335fa493122832854936d7757eb1a254e3dbf1
VitalinaLiane <vitalina.liane@gmail.com>
2016-06-12 05:26:49 +0300

    WithContact DeActivating via Email

--- db/migrate/20160612035500_add_unsubscribed_link_to_contacts.rb
+++
@@ -1,6 +0,0 @@
-class AddUnsubscribedLinkToContacts < ActiveRecord::Migration
-  def change
-    add_column :contacts, :unsubscribed_link, :string
-  end
-end
-
--- db/migrate/20160612035501_remove_unsubscribed_link_to_contacts.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUnsubscribedLinkToContacts < ActiveRecord::Migration
-  def change
-    remove_column :contacts, :unsubscribed_link
-  end
-end
-
