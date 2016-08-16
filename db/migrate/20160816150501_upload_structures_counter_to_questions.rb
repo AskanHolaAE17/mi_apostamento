3bfd557daadde8ca793b03611fd2b68641dd4e8f
VitalinaLiane <vitalina.liane@gmail.com>
2016-08-16 15:37:55 +0300

    Reloading Structures Questions - Counter

---
+++ db/migrate/20160816150501_upload_structures_counter_to_questions.rb
@@ -0,0 +1,11 @@
+class UploadStructuresOklToKlAndOlToQuestions < ActiveRecord::Migration
+  
+  
+  @count = 1
+  Question.all.where(test_id: '1').each do |f|
+    f.update_attribute :number_of_question, @count
+    @count = @count + 1
+  end
+  
+end
+
