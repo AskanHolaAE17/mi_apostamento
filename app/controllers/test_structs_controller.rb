06b073bee4fb71ab0d72bde9366333eeec15ddff
AskanHolaAE17 <be.in.pair@gmail.com>
2017-01-14 14:30:17 +0200

    02

--- app/controllers/test_structs_controller.rb
+++ app/controllers/test_structs_controller.rb
@@ -1,5 +1,6 @@
 require 'uri'
 require 'openssl'
+require 'base64'
 class TestStructsController < ApplicationController
 
 
@@ -971,7 +972,7 @@ class TestStructsController < ApplicationController
 		    
 		    
 	all_qws_for_to_do_64 = params[:qs]
-	all_qws_for_to_do    = Base64.encode all_qws_for_to_do_64	    
+	all_qws_for_to_do    = Base64.decode64 all_qws_for_to_do_64	    
 	all_qws_for_to_do_ar = all_qws_for_to_do.split(' ')		            
         @current_question_serial_number	= (all_qws_for_to_do_ar.index(qw_number.to_s) + 1).to_s
 		    
