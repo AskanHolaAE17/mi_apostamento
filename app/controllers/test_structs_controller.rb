a1857eac962026535ab480af2483568f27576477
AskanHolaAE17 <be.in.pair@gmail.com>
2017-01-14 14:16:49 +0200

    current_q...number - Test01

--- app/controllers/test_structs_controller.rb
+++ app/controllers/test_structs_controller.rb
@@ -970,7 +970,7 @@ class TestStructsController < ApplicationController
         #@current_question_serial_number = @current_question_serial_number.to_s	
 		    
 		    
-	#all_qws_for_to_do_64 = params[:qs]
+	all_qws_for_to_do_64 = params[:qs]
 	#all_qws_for_to_do    = Base64.decode all_qws_for_to_do_64	    
 	#all_qws_for_to_do_ar = all_qws_for_to_do.split(' ')		            
         #@current_question_serial_number	= (all_qws_for_to_do_ar.index(qw_number.to_s) + 1).to_s
