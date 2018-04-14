793be3520b09012c32e2f4e180689ec29720c872
AskanHolaAE17 <be.in.pair@gmail.com>
2018-04-14 18:13:22 +0300

    Done:: ConsultantMailing for PersonalConsult Form (need antiBot for records)

--- app/mailers/user_personal_consult_mailer.rb
+++ app/mailers/user_personal_consult_mailer.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 class UserPersonalConsultMailer < ActionMailer::Base
 
-  default from: 'be-in-pair@gmail.com'
+  default from: 'be.in.pair@gmail.com'
   
   
   def b_mail_to_consult_for_first_connection(user_personal_consult)
     @user_personal_consult = user_personal_consult            
-    mail(to: 'be-in-pair@gmail.com', subject: 'Новый клиент на консультацию (психология)')
+    mail(to: 'be.in.pair@gmail.com', subject: 'Новый клиент на консультацию (психология)')
   end
 
    
