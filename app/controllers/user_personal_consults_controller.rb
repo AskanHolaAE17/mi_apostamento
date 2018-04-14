246f25f4a6c0f2fc75414937ba3c40f158827edc
AskanHolaAE17 <be.in.pair@gmail.com>
2018-04-14 17:08:34 +0300

    Protecter: theRecordFromBot while saving PersonalConsult Form (pending)

--- app/controllers/user_personal_consults_controller.rb
+++ app/controllers/user_personal_consults_controller.rb
@@ -11,42 +11,73 @@ class UserPersonalConsultsController < ApplicationController
   end
   
   
-  
-  def create
-  
-    unless '@'.in?params[:user_personal_consult][:email].to_s or params[:email].to_s.length > 3   # email must be at least: a@a || @aa || aa@
-      redirect_to '/personal_consult' 
-    else  
-  
-      user_site              = UserSite.new
-      user_personal_consult  = user_site.user_personal_consults.build(user_personal_consult_params)     
-              
-      user_site.email        = user_personal_consult.email    
-      
-      akey_full = akey
-      user_site.akey_short              = akey_full[0..akey_full.length/2]
-      akey_full = akey
-      user_personal_consult.akey_short  = akey_full[0..akey_full.length/2]
-      
-      user_site.save
-      user_personal_consult.save
+#____________________________________________________________________________________________________________________
+
+
+
+  def create   
+        
     
-      ##pendeing:
-        # NAME in form (for future emails)
-        # [email -> ] TYPE of connection (~checkboxes)
-         # with extra text field (+required)
-        # DESCR in form      
-        ### INCREMENT COOL USER PAY HANDLE BUTTON (story and count  of user_cons, common_sum of user_site) + name 
-              
+    def created_by_human
     
-      redirect_to '/'  # info page with waiting SOON message from Consulter
-  
+      return true
+      
     end
+    
+    #_____________________________________________________________________________    
+    
+    
+      
+    if created_by_human
+    
+      unless '@'.in?params[:user_personal_consult][:email].to_s or params[:email].to_s.length > 3   # email must be at least: a@a || @aa || aa@
+        redirect_to '/personal_consult' 
+        
+      #_____________________________________________________________________________
+      
+      
+      else   # for:: email must be at least: a@a || @aa || aa@
+  
+        user_site              = UserSite.new
+        user_personal_consult  = user_site.user_personal_consults.build(user_personal_consult_params)     
+              
+        user_site.email        = user_personal_consult.email    
+      
+        akey_full = akey
+        user_site.akey_short              = akey_full[0..akey_full.length/2]
+        akey_full = akey
+        user_personal_consult.akey_short  = akey_full[0..akey_full.length/2]
+      
+        user_site.save
+        user_personal_consult.save
+    
+        ##pendeing:
+          # NAME in form (for future emails)
+          # [email -> ] TYPE of connection (~checkboxes)
+           # with extra text field (+required)
+          # DESCR in form      
+          ### INCREMENT COOL USER PAY HANDLE BUTTON (story and count  of user_cons, common_sum of user_site) + name 
+              
+    
+        redirect_to '/'  # info page with waiting SOON message from Consulter
+  
+      end   # for:: email must be at least: a@a || @aa || aa@          
+      
+    #_____________________________________________________________________________
+    
+      
+    else   # for:: unless 'record is created by Human, not bot'
+    
+      # EXCEPTION
+    
+    end   # for:: unless 'record is created by Human, not bot'
+          
+    
   end
 
 
 
-#_____________________________________________________________________________________________________________________________________________    
+#____________________________________________________________________________________________________________________
 
     
   
